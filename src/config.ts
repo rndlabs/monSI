67a985b7787055c603baa6a4cc476498e65cdf4d
ldeffenb <ldeffenb@homeside.to>
2022-11-18 09:04:22 -0500

    fix: lower-case all contract IDs for safe comparisons

--- src/config.ts
+++ src/config.ts
@@ -50,13 +50,15 @@ export const chainConfig: Configs = {
 		},
 		contracts: {
 			//			redistribution: '0xF4963031E8b9f9659CB6ed35E53c031D76480EAD',	// Pre 2022/10/28 10.0.0 rc1
-			redistribution: '0x2e4ccEb9a88bcfbC7779Fa693714783A345c1343', // obsolete pre-RC4
-			redistributionRC4: '0x3174ed554247b3457922aecfcbdb17db123e519c', // correct 2022/11/14 from pkg/config/chain.go
-			stakeRegistry: '0x39fc8db5d43c68379155b41451aa38190faa3498', // correct 2022/11/14 from pkg/config/chain.go
+			redistribution:
+				'0x2e4ccEb9a88bcfbC7779Fa693714783A345c1343'.toLowerCase(), // obsolete pre-RC4
+			redistributionRC4:
+				'0x3174ed554247b3457922aecfcbdb17db123e519c'.toLowerCase(), // correct 2022/11/14 from pkg/config/chain.go
+			stakeRegistry: '0x39fc8db5d43c68379155b41451aa38190faa3498'.toLowerCase(), // correct 2022/11/14 from pkg/config/chain.go
 			stakeDeployBlock: 7951984,
-			bzzToken: '0x2aC3c1d3e24b45c6C310534Bc2Dd84B5ed576335', // correct 2022/11/14 from PostageStamp & Stake constructors
-			postageStamp: '0xf0a7e63f72a73bd339a7ee814aefa80995e532a0', // correct 2022/11/14 from pkg/config/chain.go
-			priceOracle: '0x2b308ceb1ff7398e93b008c9a9ea82ffcf6a7a24', // correct 2022/11/14 from Redistribution constructor
+			bzzToken: '0x2aC3c1d3e24b45c6C310534Bc2Dd84B5ed576335'.toLowerCase(), // correct 2022/11/14 from PostageStamp & Stake constructors
+			postageStamp: '0xf0a7e63f72a73bd339a7ee814aefa80995e532a0'.toLowerCase(), // correct 2022/11/14 from pkg/config/chain.go
+			priceOracle: '0x2b308ceb1ff7398e93b008c9a9ea82ffcf6a7a24'.toLowerCase(), // correct 2022/11/14 from Redistribution constructor
 		},
 	},
 	'100': {
@@ -75,13 +77,15 @@ export const chainConfig: Configs = {
 			revealPhaseBlocks: 152 / 4,
 		},
 		contracts: {
-			redistribution: '0xF4963031E8b9f9659CB6ed35E53c031D76480EAD', // wrong
-			redistributionRC4: '0xF4963031E8b9f9659CB6ed35E53c031D76480EAD', // wrong
-			stakeRegistry: '0x18391158435582D5bE5ac1640ab5E2825F68d3a4', // wrong
+			redistribution:
+				'0xF4963031E8b9f9659CB6ed35E53c031D76480EAD'.toLowerCase(), // wrong
+			redistributionRC4:
+				'0xF4963031E8b9f9659CB6ed35E53c031D76480EAD'.toLowerCase(), // wrong
+			stakeRegistry: '0x18391158435582D5bE5ac1640ab5E2825F68d3a4'.toLowerCase(), // wrong
 			stakeDeployBlock: 0, // wrong (duh)
-			bzzToken: '0xdBF3Ea6F5beE45c02255B2c26a16F300502F68da', // correct
-			postageStamp: '0x6a1A21ECA3aB28BE85C7Ba22b2d6eAE5907c900E', // correct
-			priceOracle: '0x0FDc5429C50e2a39066D8A94F3e2D2476fcc3b85', // correct, "xdaiContractAddress" in pkg/config/chain.go
+			bzzToken: '0xdBF3Ea6F5beE45c02255B2c26a16F300502F68da'.toLowerCase(), // correct
+			postageStamp: '0x6a1A21ECA3aB28BE85C7Ba22b2d6eAE5907c900E'.toLowerCase(), // correct
+			priceOracle: '0x0FDc5429C50e2a39066D8A94F3e2D2476fcc3b85'.toLowerCase(), // correct, "xdaiContractAddress" in pkg/config/chain.go
 		},
 	},
 }
--- src/lib/formatText.ts
+++ src/lib/formatText.ts
@@ -24,14 +24,12 @@ export function leftId(id: string, n: number, ellipses = true) {
 
 export function fmtAccount(acct: string, n = 12): string {
 	acct = acct.toLowerCase()
-	if (acct == config.contracts.redistribution.toLowerCase())
-		return 'Redistribution'
-	if (acct == config.contracts.stakeRegistry.toLowerCase())
-		return 'StakeRegistry'
-	if (acct == config.contracts.bzzToken.toLowerCase())
-		return config.units.BZZ + 'Token'
-	if (acct == config.contracts.postageStamp.toLowerCase()) return 'PostageStamp'
-	if (acct == config.contracts.priceOracle.toLowerCase()) return 'PriceOracle'
+	if (acct == config.contracts.redistribution) return 'Redistribution'
+	if (acct == config.contracts.redistributionRC4) return 'RedistributionRC4'
+	if (acct == config.contracts.stakeRegistry) return 'StakeRegistry'
+	if (acct == config.contracts.bzzToken) return config.units.BZZ + 'Token'
+	if (acct == config.contracts.postageStamp) return 'PostageStamp'
+	if (acct == config.contracts.priceOracle) return 'PriceOracle'
 
 	let r = leftId(acct, n)
 	if (SchellingGame.getInstance().isMyAccount(acct))
