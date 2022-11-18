da583ec1fdcb6e052f46250b46c3ae0fefa94fab
ldeffenb <ldeffenb@homeside.to>
2022-11-18 08:13:43 -0500

    feat: update to RC7 contracts and use lower-case for comparisons

--- src/config.ts
+++ src/config.ts
@@ -21,6 +21,7 @@ export type ChainConfig = {
 	contracts: {
 		redistribution: string
 		stakeRegistry: string
+		stakeDeployBlock: number
 		bzzToken: string
 		postageStamp: string
 		priceOracle: string
@@ -48,11 +49,13 @@ export const chainConfig: Configs = {
 		},
 		contracts: {
 			//			redistribution: '0xF4963031E8b9f9659CB6ed35E53c031D76480EAD',	// Pre 2022/10/28 10.0.0 rc1
-			redistribution: '0x2e4ccEb9a88bcfbC7779Fa693714783A345c1343',
-			stakeRegistry: '0x18391158435582D5bE5ac1640ab5E2825F68d3a4',
-			bzzToken: '0x2aC3c1d3e24b45c6C310534Bc2Dd84B5ed576335',
-			postageStamp: '0x7aAC0f092F7b961145900839Ed6d54b1980F200c',
-			priceOracle: '0x0c9de531dcb38b758fe8a2c163444a5e54ee0db2', // "goerliContractAddress" in pkg/config/chain.go
+			redistribution:
+				'0x3174ed554247b3457922aecfcbdb17db123e519c'.toLowerCase(), // correct 2022/11/14 from pkg/config/chain.go
+			stakeRegistry: '0x39fc8db5d43c68379155b41451aa38190faa3498'.toLowerCase(), // correct 2022/11/14 from pkg/config/chain.go
+			stakeDeployBlock: 7951984,
+			bzzToken: '0x2aC3c1d3e24b45c6C310534Bc2Dd84B5ed576335'.toLowerCase(), // correct 2022/11/14 from PostageStamp & Stake constructors
+			postageStamp: '0xf0a7e63f72a73bd339a7ee814aefa80995e532a0'.toLowerCase(), // correct 2022/11/14 from pkg/config/chain.go
+			priceOracle: '0x2b308ceb1ff7398e93b008c9a9ea82ffcf6a7a24'.toLowerCase(), // correct 2022/11/14 from Redistribution constructor
 		},
 	},
 	'100': {
@@ -73,6 +76,7 @@ export const chainConfig: Configs = {
 		contracts: {
 			redistribution: '0xF4963031E8b9f9659CB6ed35E53c031D76480EAD', // wrong
 			stakeRegistry: '0x18391158435582D5bE5ac1640ab5E2825F68d3a4', // wrong
+			stakeDeployBlock: 7951984,
 			bzzToken: '0xdBF3Ea6F5beE45c02255B2c26a16F300502F68da', // correct
 			postageStamp: '0x6a1A21ECA3aB28BE85C7Ba22b2d6eAE5907c900E', // correct
 			priceOracle: '0x0FDc5429C50e2a39066D8A94F3e2D2476fcc3b85', // correct, "xdaiContractAddress" in pkg/config/chain.go
